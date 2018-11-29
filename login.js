6df2ec7d3003610db100bfff676c81826b5333d3
Portfolio <marta.grzegowska@gmail.com>
2018-11-29 15:28:25 +0100

    trying to register new user

--- login.js
+++ login.js
@@ -14,6 +14,57 @@ loginform.addEventListener('submit',(e)=>{
     
 })
 
+// register form
+const registerbutton = document.querySelector('#registerbutton')
+const registerform = document.querySelector('.register-form');
+
+
+registerform.addEventListener("submit", e => {
+            e.preventDefault();
+            addNewUser();
+        });
+
+//sending data to database
+function  addNewUser(){
+    const payload = {
+        username: addNewUser.elements.username.value,
+    };
+	
+  fetch(endpoint, {
+        method :"post",
+        body: JSON.stringify(payload),
+        headers: {
+            Accept : "application/json",
+            "Content-Type":"application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(d => {
+        console.log(d);
+    })
+}
+   function getAllUsers() {
+            fetch(endpoint)
+                .then(res => res.json())
+                .then(showAllUsers)
+        }
+
+        function showAllUsers(users) {
+            users.forEach(showSingleUser)
+        }
+
+        function showSingleUser(users) {
+            const clone = template.cloneNode(true);
+          
+            clone.querySelector("p").textContent = users.username;
+            root.appendChild(clone);
+
+        }
+
+
+
+
+
 function checkLogin(username){
     fetch('https://5bff9c6d0296210013dc7df1.mockapi.io/api/v1/users?search='+username)
     .then(res=>res.json())
