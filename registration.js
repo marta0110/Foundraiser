600abb47e137be31f728fff7681a95c8edbcd9d3
mariacamb <mari11df@stud.kea.dk>
2018-12-03 16:14:25 +0100

    form fixes

--- registration.js
+++ registration.js
@@ -16,12 +16,7 @@ function  addNewUser(){
         username: registerform.elements.username.value,
         password: registerform.elements.password.value,
         email: registerform.elements.email.value,
-<<<<<<< HEAD
-		avatar: registerform.elements.pic.value,
-=======
-        avatar: registerform.elements.pic.accept
-        
->>>>>>> 97fcf689607d6c8a850dd52fb7d3c03a6a6cc447
+		avatar: registerform.elements.pic.accept,
 
     };
 	
